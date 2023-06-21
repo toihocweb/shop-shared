4291e86d63bd3f7b3f503c5f94eed7d98b8059b7
nhabui <dinhnhat.bui@zuhlke.com>
2023-06-22 00:08:44 +0700

    add auth interfaces

--- dist/auth/index.d.ts
+++ dist/auth/index.d.ts
@@ -4,3 +4,56 @@ export declare enum Permission {
     STAFF = "staff",
     CUSTOMER = "customer"
 }
+export declare class SocialLoginDto {
+    provider: string;
+    access_token: string;
+}
+export declare class ChangePasswordDto {
+    oldPassword: string;
+    newPassword: string;
+}
+export declare class ForgetPasswordDto {
+    email: string;
+}
+export declare class VerifyForgetPasswordDto {
+    email: string;
+    token: string;
+}
+export declare class ResetPasswordDto {
+    email: string;
+    token: string;
+    password: string;
+}
+export declare class AuthResponse {
+    token: string;
+    permissions: Permission[];
+}
+export declare class VerifyOtpDto {
+    otp_id: string;
+    code: string;
+    phone_number: string;
+}
+export declare class OtpResponse {
+    id: string;
+    message: string;
+    success: boolean;
+    phone_number: string;
+    provider: string;
+    is_contact_exist: boolean;
+}
+export declare class OtpDto {
+    phone_number: string;
+}
+export declare class OtpLoginDto {
+    otp_id: string;
+    code: string;
+    phone_number: string;
+    name?: string;
+    email?: string;
+}
+export declare class AuthPayloadResponse {
+    id: number;
+    username: string;
+    email: string;
+    permissions: Permission[];
+}
--- dist/auth/index.js
+++ dist/auth/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Permission = void 0;
+exports.AuthPayloadResponse = exports.OtpLoginDto = exports.OtpDto = exports.OtpResponse = exports.VerifyOtpDto = exports.AuthResponse = exports.ResetPasswordDto = exports.VerifyForgetPasswordDto = exports.ForgetPasswordDto = exports.ChangePasswordDto = exports.SocialLoginDto = exports.Permission = void 0;
 var Permission;
 (function (Permission) {
     Permission["SUPER_ADMIN"] = "super_admin";
@@ -8,4 +8,37 @@ var Permission;
     Permission["STAFF"] = "staff";
     Permission["CUSTOMER"] = "customer";
 })(Permission || (exports.Permission = Permission = {}));
+class SocialLoginDto {
+}
+exports.SocialLoginDto = SocialLoginDto;
+class ChangePasswordDto {
+}
+exports.ChangePasswordDto = ChangePasswordDto;
+class ForgetPasswordDto {
+}
+exports.ForgetPasswordDto = ForgetPasswordDto;
+class VerifyForgetPasswordDto {
+}
+exports.VerifyForgetPasswordDto = VerifyForgetPasswordDto;
+class ResetPasswordDto {
+}
+exports.ResetPasswordDto = ResetPasswordDto;
+class AuthResponse {
+}
+exports.AuthResponse = AuthResponse;
+class VerifyOtpDto {
+}
+exports.VerifyOtpDto = VerifyOtpDto;
+class OtpResponse {
+}
+exports.OtpResponse = OtpResponse;
+class OtpDto {
+}
+exports.OtpDto = OtpDto;
+class OtpLoginDto {
+}
+exports.OtpLoginDto = OtpLoginDto;
+class AuthPayloadResponse {
+}
+exports.AuthPayloadResponse = AuthPayloadResponse;
 //# sourceMappingURL=index.js.map
