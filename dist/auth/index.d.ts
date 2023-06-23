c6acd798b4fe2f15e65d2d15090642d25457fd8a
nhabui <dinhnhat.bui@zuhlke.com>
2023-06-23 22:56:01 +0700

    new auth

--- dist/auth/index.d.ts
+++ dist/auth/index.d.ts
@@ -1,59 +1,192 @@
+import { z } from "zod";
 export declare enum Permission {
-    SUPER_ADMIN = "super_admin",
-    STORE_OWNER = "store_owner",
-    STAFF = "staff",
-    CUSTOMER = "customer"
+  SUPER_ADMIN = "super_admin",
+  STORE_OWNER = "store_owner",
+  STAFF = "staff",
+  CUSTOMER = "customer",
 }
-export declare class SocialLoginDto {
-    provider: string;
-    access_token: string;
-}
-export declare class ChangePasswordDto {
-    oldPassword: string;
-    newPassword: string;
-}
-export declare class ForgetPasswordDto {
-    email: string;
-}
-export declare class VerifyForgetPasswordDto {
-    email: string;
-    token: string;
-}
-export declare class ResetPasswordDto {
-    email: string;
-    token: string;
-    password: string;
-}
+export declare const SocialLoginSchema: z.ZodObject<
+  {
+    provider: z.ZodString;
+    access_token: z.ZodString;
+  },
+  "strip",
+  z.ZodTypeAny,
+  {
+    provider?: string;
+    access_token?: string;
+  },
+  {
+    provider?: string;
+    access_token?: string;
+  }
+>;
+export type SocialLoginDto = z.infer<typeof SocialLoginSchema>;
+export declare const ForgetPasswordSchema: z.ZodObject<
+  {
+    email: z.ZodString;
+  },
+  "strip",
+  z.ZodTypeAny,
+  {
+    email?: string;
+  },
+  {
+    email?: string;
+  }
+>;
+export type ForgetPasswordDto = z.infer<typeof ForgetPasswordSchema>;
+export declare const VerifyForgetPasswordSchema: z.ZodObject<
+  {
+    email: z.ZodString;
+    token: z.ZodString;
+  },
+  "strip",
+  z.ZodTypeAny,
+  {
+    email?: string;
+    token?: string;
+  },
+  {
+    email?: string;
+    token?: string;
+  }
+>;
+export type VerifyForgetPasswordDto = z.infer<
+  typeof VerifyForgetPasswordSchema
+>;
+export declare const ResetPasswordSchema: z.ZodObject<
+  {
+    email: z.ZodString;
+    token: z.ZodString;
+    password: z.ZodString;
+  },
+  "strip",
+  z.ZodTypeAny,
+  {
+    email?: string;
+    token?: string;
+    password?: string;
+  },
+  {
+    email?: string;
+    token?: string;
+    password?: string;
+  }
+>;
+export type ResetPasswordDto = z.infer<typeof ResetPasswordSchema>;
 export declare class AuthResponse {
-    token: string;
-    permissions: Permission[];
+  token: string;
+  permissions: Permission[];
 }
-export declare class VerifyOtpDto {
-    otp_id: string;
-    code: string;
-    phone_number: string;
-}
+export declare const VerifyOtpSchema: z.ZodObject<
+  {
+    otp_id: z.ZodString;
+    code: z.ZodString;
+    phone_number: z.ZodString;
+  },
+  "strip",
+  z.ZodTypeAny,
+  {
+    otp_id?: string;
+    code?: string;
+    phone_number?: string;
+  },
+  {
+    otp_id?: string;
+    code?: string;
+    phone_number?: string;
+  }
+>;
+export type VerifyOtpDto = z.infer<typeof VerifyOtpSchema>;
 export declare class OtpResponse {
-    id: string;
-    message: string;
-    success: boolean;
-    phone_number: string;
-    provider: string;
-    is_contact_exist: boolean;
+  id: string;
+  message: string;
+  success: boolean;
+  phone_number: string;
+  provider: string;
+  is_contact_exist: boolean;
 }
-export declare class OtpDto {
-    phone_number: string;
+export declare const OtpSchema: z.ZodObject<
+  {
+    phone_number: z.ZodString;
+  },
+  "strip",
+  z.ZodTypeAny,
+  {
+    phone_number?: string;
+  },
+  {
+    phone_number?: string;
+  }
+>;
+export type OtpDto = z.infer<typeof OtpSchema>;
+export declare class AuthPayloadResponse {
+  id: string;
+  username: string;
+  email: string;
+  permissions: Permission[];
 }
-export declare class OtpLoginDto {
-    otp_id: string;
-    code: string;
-    phone_number: string;
+export declare const CreateUserDtoSchema: z.ZodObject<
+  {
+    username: z.ZodString;
+    email: z.ZodString;
+    password: z.ZodString;
+  },
+  "strip",
+  z.ZodTypeAny,
+  {
+    username?: string;
+    email?: string;
+    password?: string;
+  },
+  {
+    username?: string;
+    email?: string;
+    password?: string;
+  }
+>;
+export type CreateUserDto = z.infer<typeof CreateUserDtoSchema>;
+export declare const ChangePasswordSchema: z.ZodObject<
+  {
+    oldPassword: z.ZodString;
+    newPassword: z.ZodString;
+  },
+  "strip",
+  z.ZodTypeAny,
+  {
+    oldPassword?: string;
+    newPassword?: string;
+  },
+  {
+    oldPassword?: string;
+    newPassword?: string;
+  }
+>;
+export type ChangePasswordDto = z.infer<typeof ChangePasswordSchema>;
+export declare const OtpLoginSchema: z.ZodObject<
+  {
+    otp_id: z.ZodString;
+    code: z.ZodString;
+    phone_number: z.ZodString;
+    name: z.ZodOptional<z.ZodString>;
+    email: z.ZodOptional<z.ZodString>;
+  },
+  "strip",
+  z.ZodTypeAny,
+  {
+    otp_id?: string;
+    code?: string;
+    phone_number?: string;
     name?: string;
     email?: string;
-}
-export declare class AuthPayloadResponse {
-    id: string;
-    username: string;
-    email: string;
-    permissions: Permission[];
-}
+  },
+  {
+    otp_id?: string;
+    code?: string;
+    phone_number?: string;
+    name?: string;
+    email?: string;
+  }
+>;
+export type OtpLoginDto = z.infer<typeof OtpLoginSchema>;
--- dist/auth/index.js
+++ dist/auth/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AuthPayloadResponse = exports.OtpLoginDto = exports.OtpDto = exports.OtpResponse = exports.VerifyOtpDto = exports.AuthResponse = exports.ResetPasswordDto = exports.VerifyForgetPasswordDto = exports.ForgetPasswordDto = exports.ChangePasswordDto = exports.SocialLoginDto = exports.Permission = void 0;
+exports.OtpLoginSchema = exports.ChangePasswordSchema = exports.CreateUserDtoSchema = exports.AuthPayloadResponse = exports.OtpSchema = exports.OtpResponse = exports.VerifyOtpSchema = exports.AuthResponse = exports.ResetPasswordSchema = exports.VerifyForgetPasswordSchema = exports.ForgetPasswordSchema = exports.SocialLoginSchema = exports.Permission = void 0;
+const zod_1 = require("zod");
 var Permission;
 (function (Permission) {
     Permission["SUPER_ADMIN"] = "super_admin";
@@ -8,37 +9,53 @@ var Permission;
     Permission["STAFF"] = "staff";
     Permission["CUSTOMER"] = "customer";
 })(Permission || (exports.Permission = Permission = {}));
-class SocialLoginDto {
-}
-exports.SocialLoginDto = SocialLoginDto;
-class ChangePasswordDto {
-}
-exports.ChangePasswordDto = ChangePasswordDto;
-class ForgetPasswordDto {
-}
-exports.ForgetPasswordDto = ForgetPasswordDto;
-class VerifyForgetPasswordDto {
-}
-exports.VerifyForgetPasswordDto = VerifyForgetPasswordDto;
-class ResetPasswordDto {
-}
-exports.ResetPasswordDto = ResetPasswordDto;
+exports.SocialLoginSchema = zod_1.z.object({
+    provider: zod_1.z.string(),
+    access_token: zod_1.z.string(),
+});
+exports.ForgetPasswordSchema = zod_1.z.object({
+    email: zod_1.z.string().email(),
+});
+exports.VerifyForgetPasswordSchema = zod_1.z.object({
+    email: zod_1.z.string().email(),
+    token: zod_1.z.string(),
+});
+exports.ResetPasswordSchema = zod_1.z.object({
+    email: zod_1.z.string().email(),
+    token: zod_1.z.string(),
+    password: zod_1.z.string().min(6),
+});
 class AuthResponse {
 }
 exports.AuthResponse = AuthResponse;
-class VerifyOtpDto {
-}
-exports.VerifyOtpDto = VerifyOtpDto;
+exports.VerifyOtpSchema = zod_1.z.object({
+    otp_id: zod_1.z.string(),
+    code: zod_1.z.string(),
+    phone_number: zod_1.z.string(),
+});
 class OtpResponse {
 }
 exports.OtpResponse = OtpResponse;
-class OtpDto {
-}
-exports.OtpDto = OtpDto;
-class OtpLoginDto {
-}
-exports.OtpLoginDto = OtpLoginDto;
+exports.OtpSchema = zod_1.z.object({
+    phone_number: zod_1.z.string(),
+});
 class AuthPayloadResponse {
 }
 exports.AuthPayloadResponse = AuthPayloadResponse;
+exports.CreateUserDtoSchema = zod_1.z.object({
+    username: zod_1.z.string().min(3),
+    email: zod_1.z.string().email(),
+    password: zod_1.z.string().min(6),
+});
+exports.ChangePasswordSchema = zod_1.z.object({
+    oldPassword: zod_1.z.string().min(6),
+    newPassword: zod_1.z.string().min(6),
+});
+exports.OtpLoginSchema = zod_1.z.object({
+    otp_id: zod_1.z.string(),
+    code: zod_1.z.string(),
+    phone_number: zod_1.z.string(),
+    name: zod_1.z.string().optional(),
+    email: zod_1.z.string().optional(),
+});
 //# sourceMappingURL=index.js.map
