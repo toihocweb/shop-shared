a6662b8e846a80b130d97b65b048e6763de48814
nhabui <dinhnhat.bui@zuhlke.com>
2023-06-25 10:46:55 +0700

    update addres

--- dist/addresses/index.d.ts
+++ dist/addresses/index.d.ts
@@ -3,37 +3,116 @@ export declare enum AddressType {
     BILLING = "billing",
     SHIPPING = "shipping"
 }
+export declare const UserAddress: z.ZodObject<{
+    country: z.ZodString;
+    state: z.ZodString;
+    city: z.ZodString;
+    street_address: z.ZodString;
+    zip: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    country: string;
+    state: string;
+    city: string;
+    street_address: string;
+    zip: string;
+}, {
+    country: string;
+    state: string;
+    city: string;
+    street_address: string;
+    zip: string;
+}>;
 export declare const CreateAddressSchema: z.ZodObject<{
     title: z.ZodString;
-    type: z.ZodString;
+    type: z.ZodEnum<[AddressType.BILLING, AddressType.SHIPPING]>;
     default: z.ZodDefault<z.ZodBoolean>;
-    address: z.ZodEnum<[AddressType.BILLING, AddressType.SHIPPING]>;
+    address: z.ZodObject<{
+        country: z.ZodString;
+        state: z.ZodString;
+        city: z.ZodString;
+        street_address: z.ZodString;
+        zip: z.ZodString;
+    }, "strip", z.ZodTypeAny, {
+        country: string;
+        state: string;
+        city: string;
+        street_address: string;
+        zip: string;
+    }, {
+        country: string;
+        state: string;
+        city: string;
+        street_address: string;
+        zip: string;
+    }>;
 }, "strip", z.ZodTypeAny, {
+    type: AddressType;
     title: string;
-    type: string;
     default: boolean;
-    address: AddressType;
+    address: {
+        country: string;
+        state: string;
+        city: string;
+        street_address: string;
+        zip: string;
+    };
 }, {
+    type: AddressType;
     title: string;
-    type: string;
-    address: AddressType;
+    address: {
+        country: string;
+        state: string;
+        city: string;
+        street_address: string;
+        zip: string;
+    };
     default?: boolean | undefined;
 }>;
 export type CreateAddressDto = z.infer<typeof CreateAddressSchema>;
 export declare const UpdateAddressSchema: z.ZodObject<{
     title: z.ZodOptional<z.ZodString>;
-    type: z.ZodOptional<z.ZodString>;
+    type: z.ZodOptional<z.ZodEnum<[AddressType.BILLING, AddressType.SHIPPING]>>;
     default: z.ZodOptional<z.ZodDefault<z.ZodBoolean>>;
-    address: z.ZodOptional<z.ZodEnum<[AddressType.BILLING, AddressType.SHIPPING]>>;
+    address: z.ZodOptional<z.ZodObject<{
+        country: z.ZodString;
+        state: z.ZodString;
+        city: z.ZodString;
+        street_address: z.ZodString;
+        zip: z.ZodString;
+    }, "strip", z.ZodTypeAny, {
+        country: string;
+        state: string;
+        city: string;
+        street_address: string;
+        zip: string;
+    }, {
+        country: string;
+        state: string;
+        city: string;
+        street_address: string;
+        zip: string;
+    }>>;
 }, "strip", z.ZodTypeAny, {
     title?: string | undefined;
-    type?: string | undefined;
+    type?: AddressType | undefined;
     default?: boolean | undefined;
-    address?: AddressType | undefined;
+    address?: {
+        country: string;
+        state: string;
+        city: string;
+        street_address: string;
+        zip: string;
+    } | undefined;
 }, {
     title?: string | undefined;
-    type?: string | undefined;
+    type?: AddressType | undefined;
     default?: boolean | undefined;
-    address?: AddressType | undefined;
+    address?: {
+        country: string;
+        state: string;
+        city: string;
+        street_address: string;
+        zip: string;
+    } | undefined;
 }>;
 export type UpdateAddressDto = z.infer<typeof UpdateAddressSchema>;
--- dist/addresses/index.js
+++ dist/addresses/index.js
@@ -1,17 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateAddressSchema = exports.CreateAddressSchema = exports.AddressType = void 0;
+exports.UpdateAddressSchema = exports.CreateAddressSchema = exports.UserAddress = exports.AddressType = void 0;
 const zod_1 = require("zod");
 var AddressType;
 (function (AddressType) {
     AddressType["BILLING"] = "billing";
     AddressType["SHIPPING"] = "shipping";
 })(AddressType || (exports.AddressType = AddressType = {}));
+exports.UserAddress = zod_1.z.object({
+    country: zod_1.z.string().nonempty(),
+    state: zod_1.z.string().nonempty(),
+    city: zod_1.z.string().nonempty(),
+    street_address: zod_1.z.string().nonempty(),
+    zip: zod_1.z.string().nonempty(),
+});
 exports.CreateAddressSchema = zod_1.z.object({
     title: zod_1.z.string().nonempty(),
-    type: zod_1.z.string().nonempty(),
+    type: zod_1.z.enum([AddressType.BILLING, AddressType.SHIPPING]),
     default: zod_1.z.boolean().default(false),
-    address: zod_1.z.enum([AddressType.BILLING, AddressType.SHIPPING]),
+    address: exports.UserAddress,
 });
 exports.UpdateAddressSchema = exports.CreateAddressSchema.partial();
 //# sourceMappingURL=index.js.map
