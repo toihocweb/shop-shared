3c77afb613892f4221aca54a4271e64bb63084f0
nhabui <dinhnhat.bui@zuhlke.com>
2023-06-25 11:13:29 +0700

    update address

--- dist/addresses/index.d.ts
+++ dist/addresses/index.d.ts
@@ -3,69 +3,32 @@ export declare enum AddressType {
     BILLING = "billing",
     SHIPPING = "shipping"
 }
-export declare const UserAddress: z.ZodObject<{
+export declare const CreateAddressSchema: z.ZodObject<{
+    title: z.ZodString;
+    type: z.ZodEnum<[AddressType.BILLING, AddressType.SHIPPING]>;
+    default: z.ZodDefault<z.ZodBoolean>;
     country: z.ZodString;
     state: z.ZodString;
     city: z.ZodString;
     street_address: z.ZodString;
     zip: z.ZodString;
 }, "strip", z.ZodTypeAny, {
-    country: string;
-    state: string;
-    city: string;
-    street_address: string;
-    zip: string;
-}, {
-    country: string;
-    state: string;
-    city: string;
-    street_address: string;
-    zip: string;
-}>;
-export declare const CreateAddressSchema: z.ZodObject<{
-    title: z.ZodString;
-    type: z.ZodEnum<[AddressType.BILLING, AddressType.SHIPPING]>;
-    default: z.ZodDefault<z.ZodBoolean>;
-    address: z.ZodObject<{
-        country: z.ZodString;
-        state: z.ZodString;
-        city: z.ZodString;
-        street_address: z.ZodString;
-        zip: z.ZodString;
-    }, "strip", z.ZodTypeAny, {
-        country: string;
-        state: string;
-        city: string;
-        street_address: string;
-        zip: string;
-    }, {
-        country: string;
-        state: string;
-        city: string;
-        street_address: string;
-        zip: string;
-    }>;
-}, "strip", z.ZodTypeAny, {
-    type: AddressType;
     title: string;
+    type: AddressType;
     default: boolean;
-    address: {
-        country: string;
-        state: string;
-        city: string;
-        street_address: string;
-        zip: string;
-    };
+    country: string;
+    state: string;
+    city: string;
+    street_address: string;
+    zip: string;
 }, {
-    type: AddressType;
     title: string;
-    address: {
-        country: string;
-        state: string;
-        city: string;
-        street_address: string;
-        zip: string;
-    };
+    type: AddressType;
+    country: string;
+    state: string;
+    city: string;
+    street_address: string;
+    zip: string;
     default?: boolean | undefined;
 }>;
 export type CreateAddressDto = z.infer<typeof CreateAddressSchema>;
@@ -73,46 +36,28 @@ export declare const UpdateAddressSchema: z.ZodObject<{
     title: z.ZodOptional<z.ZodString>;
     type: z.ZodOptional<z.ZodEnum<[AddressType.BILLING, AddressType.SHIPPING]>>;
     default: z.ZodOptional<z.ZodDefault<z.ZodBoolean>>;
-    address: z.ZodOptional<z.ZodObject<{
-        country: z.ZodString;
-        state: z.ZodString;
-        city: z.ZodString;
-        street_address: z.ZodString;
-        zip: z.ZodString;
-    }, "strip", z.ZodTypeAny, {
-        country: string;
-        state: string;
-        city: string;
-        street_address: string;
-        zip: string;
-    }, {
-        country: string;
-        state: string;
-        city: string;
-        street_address: string;
-        zip: string;
-    }>>;
+    country: z.ZodOptional<z.ZodString>;
+    state: z.ZodOptional<z.ZodString>;
+    city: z.ZodOptional<z.ZodString>;
+    street_address: z.ZodOptional<z.ZodString>;
+    zip: z.ZodOptional<z.ZodString>;
 }, "strip", z.ZodTypeAny, {
     title?: string | undefined;
     type?: AddressType | undefined;
     default?: boolean | undefined;
-    address?: {
-        country: string;
-        state: string;
-        city: string;
-        street_address: string;
-        zip: string;
-    } | undefined;
+    country?: string | undefined;
+    state?: string | undefined;
+    city?: string | undefined;
+    street_address?: string | undefined;
+    zip?: string | undefined;
 }, {
     title?: string | undefined;
     type?: AddressType | undefined;
     default?: boolean | undefined;
-    address?: {
-        country: string;
-        state: string;
-        city: string;
-        street_address: string;
-        zip: string;
-    } | undefined;
+    country?: string | undefined;
+    state?: string | undefined;
+    city?: string | undefined;
+    street_address?: string | undefined;
+    zip?: string | undefined;
 }>;
 export type UpdateAddressDto = z.infer<typeof UpdateAddressSchema>;
