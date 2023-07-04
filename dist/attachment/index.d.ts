71eb3f25f167ea980aee6dc8b641819b0867dff8
nhabui <dinhnhat.bui@zuhlke.com>
2023-07-04 22:07:24 +0700

    add attachment fields

--- dist/attachment/index.d.ts
+++ dist/attachment/index.d.ts
@@ -1,20 +1,36 @@
 import { z } from "zod";
 declare const AttachmentField: {
-    readonly AVATAR: "avatar";
-    readonly COVER: "cover";
+    readonly USER_AVATAR: "user.avatar";
+    readonly CATEGORY_IMAGE: "category.image";
+    readonly TYPE_IMAGE: "type.image";
+    readonly BANNER_IMAGE: "banner.image";
+    readonly PROMOTION_SLIDER: "promotion.slider";
+    readonly MENU_IMAGE: "menu.image";
+    readonly COVER_IMAGE: "cover.image";
+    readonly TAG_IMAGE: "tag.image";
+    readonly PRODUCT_IMAGE: "product.image";
+    readonly PRODUCT_GALLERY: "product.gallery";
 };
 export type AttachmentField = (typeof AttachmentField)[keyof typeof AttachmentField];
 export declare const AttachmentSchema: z.ZodObject<{
     attachment: z.ZodAny;
     field: z.ZodNativeEnum<{
-        readonly AVATAR: "avatar";
-        readonly COVER: "cover";
+        readonly USER_AVATAR: "user.avatar";
+        readonly CATEGORY_IMAGE: "category.image";
+        readonly TYPE_IMAGE: "type.image";
+        readonly BANNER_IMAGE: "banner.image";
+        readonly PROMOTION_SLIDER: "promotion.slider";
+        readonly MENU_IMAGE: "menu.image";
+        readonly COVER_IMAGE: "cover.image";
+        readonly TAG_IMAGE: "tag.image";
+        readonly PRODUCT_IMAGE: "product.image";
+        readonly PRODUCT_GALLERY: "product.gallery";
     }>;
 }, "strip", z.ZodTypeAny, {
-    field: "avatar" | "cover";
+    field: "user.avatar" | "category.image" | "type.image" | "banner.image" | "promotion.slider" | "menu.image" | "cover.image" | "tag.image" | "product.image" | "product.gallery";
     attachment?: any;
 }, {
-    field: "avatar" | "cover";
+    field: "user.avatar" | "category.image" | "type.image" | "banner.image" | "promotion.slider" | "menu.image" | "cover.image" | "tag.image" | "product.image" | "product.gallery";
     attachment?: any;
 }>;
 export {};
--- dist/attachment/index.js
+++ dist/attachment/index.js
@@ -3,8 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AttachmentSchema = void 0;
 const zod_1 = require("zod");
 const AttachmentField = {
-    AVATAR: "avatar",
-    COVER: "cover",
+    USER_AVATAR: "user.avatar",
+    CATEGORY_IMAGE: "category.image",
+    TYPE_IMAGE: "type.image",
+    BANNER_IMAGE: "banner.image",
+    PROMOTION_SLIDER: "promotion.slider",
+    MENU_IMAGE: "menu.image",
+    COVER_IMAGE: "cover.image",
+    TAG_IMAGE: "tag.image",
+    PRODUCT_IMAGE: "product.image",
+    PRODUCT_GALLERY: "product.gallery",
 };
 exports.AttachmentSchema = zod_1.z.object({
     attachment: zod_1.z.any(),
