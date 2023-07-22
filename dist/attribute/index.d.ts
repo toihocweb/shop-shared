c2674c7a9d2fe1bfb8fdd20b719e2a062a43ddca
nhabui <dinhnhat.bui@zuhlke.com>
2023-07-22 09:49:13 +0700

    add attribute

---
+++ dist/attribute/index.d.ts
@@ -0,0 +1,76 @@
+import { z } from "zod";
+export declare const AttributeValueSchema: z.ZodObject<{
+    value: z.ZodString;
+    meta: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    value: string;
+    meta?: string | undefined;
+}, {
+    value: string;
+    meta?: string | undefined;
+}>;
+export declare const AttributeSchema: z.ZodObject<{
+    name: z.ZodString;
+    values: z.ZodArray<z.ZodObject<{
+        value: z.ZodString;
+        meta: z.ZodOptional<z.ZodString>;
+    }, "strip", z.ZodTypeAny, {
+        value: string;
+        meta?: string | undefined;
+    }, {
+        value: string;
+        meta?: string | undefined;
+    }>, "atleastone">;
+}, "strip", z.ZodTypeAny, {
+    values: [{
+        value: string;
+        meta?: string | undefined;
+    }, ...{
+        value: string;
+        meta?: string | undefined;
+    }[]];
+    name: string;
+}, {
+    values: [{
+        value: string;
+        meta?: string | undefined;
+    }, ...{
+        value: string;
+        meta?: string | undefined;
+    }[]];
+    name: string;
+}>;
+export declare const AttributeUpdateSchema: z.ZodObject<{
+    name: z.ZodOptional<z.ZodString>;
+    values: z.ZodOptional<z.ZodArray<z.ZodObject<{
+        value: z.ZodString;
+        meta: z.ZodOptional<z.ZodString>;
+    }, "strip", z.ZodTypeAny, {
+        value: string;
+        meta?: string | undefined;
+    }, {
+        value: string;
+        meta?: string | undefined;
+    }>, "atleastone">>;
+}, "strip", z.ZodTypeAny, {
+    name?: string | undefined;
+    values?: [{
+        value: string;
+        meta?: string | undefined;
+    }, ...{
+        value: string;
+        meta?: string | undefined;
+    }[]] | undefined;
+}, {
+    name?: string | undefined;
+    values?: [{
+        value: string;
+        meta?: string | undefined;
+    }, ...{
+        value: string;
+        meta?: string | undefined;
+    }[]] | undefined;
+}>;
+export type AttributeValueDto = z.infer<typeof AttributeValueSchema>;
+export type AttributeDto = z.infer<typeof AttributeSchema>;
+export type AttributeUpdateDto = z.infer<typeof AttributeUpdateSchema>;
---
+++ dist/attribute/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AttributeUpdateSchema = exports.AttributeSchema = exports.AttributeValueSchema = void 0;
+const zod_1 = require("zod");
+exports.AttributeValueSchema = zod_1.z.object({
+    value: zod_1.z.string().nonempty(),
+    meta: zod_1.z.string().optional(),
+});
+exports.AttributeSchema = zod_1.z.object({
+    name: zod_1.z.string().nonempty(),
+    values: zod_1.z.array(exports.AttributeValueSchema).nonempty(),
+});
+exports.AttributeUpdateSchema = exports.AttributeSchema.partial();
+//# sourceMappingURL=index.js.map
